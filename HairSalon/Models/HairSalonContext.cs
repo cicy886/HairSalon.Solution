b5523bf26f13b0b0c8412ff5c1e20b7249b8a15d
Sisi Vieira <cicy886@gmail.com>
2021-10-09 14:45:16 -0700

    add Stylist Dbset, add OnConfiguring method to enable lazyloading

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -2,10 +2,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Models
 {
-  public class HairSalonContext : DbContext
-  {
-    public DbSet<Client> Clients { get; set; }
+    public class HairSalonContext : DbContext
+    {
+        public DbSet<Stylist> Stylists { get; set; }
 
-    public HairSalonContext(DbContextOptions options) : base(options) { }
-  }
+        public DbSet<Client> Clients { get; set; }
+
+        public HairSalonContext(DbContextOptions options) :
+            base(options)
+        {
+        }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder
+        )
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
 }
