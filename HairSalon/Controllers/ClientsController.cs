293fca8fb5883b241050261ab18459db285c931a
Sisi Vieira <cicy886@gmail.com>
2021-10-09 13:54:38 -0700

    add route to update the client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using HairSalon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Controllers
 {
@@ -35,8 +36,23 @@ namespace HairSalon.Controllers
 
         public ActionResult Details(int id)
         {
-            Client thisClient = _db.Clinets.FirstOrDefault(client => client.ClientId == id);
+            Client thisClient =
+                _db.Clinets.FirstOrDefault(client => client.ClientId == id);
             return View(thisClient);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+            return View(thisClient);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            _db.Entry(client).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
