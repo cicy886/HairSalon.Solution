885d31b70d9d52aca32508f71b5a10f01e83c03d
Sisi Vieira <cicy886@gmail.com>
2021-10-09 13:01:00 -0700

    add Creat actions: GET to displaythe from, Post to manage form submission

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,23 +1,36 @@
-using Microsoft.AspNetCore.Mvc;
-using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HairSalon.Controllers
 {
-  public class ClientsController : Controller
-  {
-    private readonly HairSalonContext _db;
-
-    public ClientsController(HairSalonContext db)
+    public class ClientsController : Controller
     {
-      _db = db;
-    }
+        private readonly HairSalonContext _db;
 
-    public ActionResult Index()
-    {
-      List<Client> model = _db.Clients.ToList();
-      return View(model);
+        public ClientsController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Client> model = _db.Clients.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            _db.Clients.Add (client);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
